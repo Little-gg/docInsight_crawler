cba005d8e08e28eb1ce6e0307381cafdabed583c
Little-gg <little.gg.talk@gmail.com>
2017-04-17 16:00:09 +0800

    log current scrap page

--- judgement/judgement/spiders/beijing.py
+++ judgement/judgement/spiders/beijing.py
@@ -6,6 +6,9 @@ from judgement.items import JudgementItem
 import re
 import time
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 
 class BeijingSpider(Spider):
@@ -17,49 +20,61 @@ class BeijingSpider(Spider):
         # Need to install chromedriver first
         # brew install chromedriver on MacOS
         self.driver = webdriver.Chrome()
+        self.sub_driver = webdriver.Chrome()
 
     def __del__(self):
-        self.driver.close()
+        # self.driver.close()
+        # self.sub_driver.close()
+        pass
 
     def start_requests(self):
-        yield Request(url=self.start_url + '/cpws/index.htm', callback=self.parse_judgements)
+        # yield Request(url=self.start_url + '/cpws/index.htm', callback=self.parse_judgements)
+        yield Request(url=self.load_current_status(), callback=self.parse_judgements)
 
     def parse_judgements(self, response):
         self.driver.get(response.url)
 
         while True:
             try:
-                next_page = self.driver.find_element_by_xpath("//span[@class='sp_next']")
-                next_page.click()
+                self.save_current_status(self.driver.current_url)
                 self.check_pause(self.driver)
                 self.find_yzm(self.driver)
                 urls_elements = self.driver.find_elements_by_xpath("//ul[@class='ul_news_long']/li/a")
                 for url_element in urls_elements:
                     url = url_element.get_attribute("href")
                     yield Request(url=url, callback=self.parse_content)
-            except:
+
+                # Go to next page
+                try:
+                    element = WebDriverWait(self.driver, 10).until(
+                        EC.presence_of_element_located((By.CLASS_NAME, "sp_next"))
+                    )
+                except Exception as e:
+                    raise e
+                next_page = self.driver.find_element_by_xpath("//span[@class='sp_next']")
+                next_page.click()
+            except Exception as e:
+                print e
                 break
 
-        self.driver.close()
+        # self.driver.close()
 
     def parse_content(self, response):
-        driver = webdriver.Chrome()
-        driver.get(response.url)
-        self.check_pause(driver)
-        self.find_yzm(driver)
-        title = driver.find_element_by_xpath("//div[@class='article_hd']/h3").text
+        self.sub_driver.get(response.url)
+        self.check_pause(self.sub_driver)
+        self.find_yzm(self.sub_driver)
+        title = self.sub_driver.find_element_by_xpath("//div[@class='article_hd']/h3").text
         try:
-            p_date = driver.find_element_by_xpath("//p[@class='p_date']").text.split(u'：')[1]
+            p_date = self.sub_driver.find_element_by_xpath("//p[@class='p_date']").text.split(u'：')[1]
             p_date = time.mktime(time.strptime(p_date, "%b %d, %Y"))
         except:
             p_date = 0
-        content = driver.find_element_by_xpath("//div[@class='article_con']").text
+        content = self.sub_driver.find_element_by_xpath("//div[@class='article_con']").text
         judgement_item = JudgementItem()
         judgement_item["url"] = response.url
         judgement_item["title"] = title
         judgement_item["publish_date"] = p_date
         judgement_item["content"] = content
-        driver.close()
         yield judgement_item
 
     def find_yzm(self, driver):
@@ -86,3 +101,13 @@ class BeijingSpider(Spider):
             raw_input("访问频率太高，等待...")
         except:
             pass
+
+    def load_current_status(self):
+        with open('current_status.url', 'r') as f:
+            url = f.read()
+            print url
+            return url
+
+    def save_current_status(self, url):
+        with open('current_status.url', 'w') as f:
+            f.write(url)
